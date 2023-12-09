54e29c236225c5012ad170707c7e3e3059301f00
Dark <darkgrouptw@gmail.com>
2023-12-09 21:20:17 +0800

    Update Google Translate python api

--- Test.py
+++ Test.py
@@ -2,15 +2,63 @@ import requests
 import json
 
 # Read File
-def ReadFile(pPath):
-    f = open(pPath, "r")
+def __ReadFile(pPath):
+    f = open(pPath, 'r')
     data = f.read()
-    print(data)
-    j = json.load(str(data))
-    print (j)
+    j = json.loads(str(data))
     f.close()
     return j
 
+BaseURL = 'https://translation.googleapis.com/language/translate/v2'
+def GetSupportLanguage(pKey):
+    formData = { 'key': pKey, 'target': 'zh-TW' }
+    try:
+        response = requests.post(BaseURL + '/languages', formData)
+        return json.loads(response.text)['data']
+    except:
+        print('GetSupportLanguage(): Something went wrong')
+    return None
 
-ReadFile("GoogleAPIKey/GoogleAPIKey.box")
+def DetectLanguage(pKey, pText):
+    formData = { 'key': pKey, 'q': pText }
+    try:
+        response = requests.post(BaseURL + '/detect', formData)
+        # Example of return
+        # {'data': {'detections': [[{'confidence': 1, 'isReliable': False, 'language': 'zh-TW'}]]}}
+        return json.loads(response.text)['data']['detections'][0][0]['language']
+    except:
+        print('DetectLanguage(): Something went wrong')
+    return None
 
+def Translate(pKey, pText, pSourceLanguage, pTargetLanguage):
+    formData = { 'key': pKey, 'q': pText, 'source': pSourceLanguage, 'target': pTargetLanguage, 'format': 'text' }
+    try:
+        response = requests.post(BaseURL, formData)
+        # Example of return
+        # {'data': {'translations': [{'translatedText': 'pText'}]}}
+        return json.loads(response.text)['data']['translations'][0]['translatedText']
+    except:
+        print('Translate(): Something went wrong')
+    return None
+
+JsonData = __ReadFile('GoogleAPIKey/GoogleAPIKey.box')
+
+# Google Translate V2
+# 1. Get Support Language
+# 2. Detect Language
+# 3. Translate
+key = JsonData['Key']
+result1 = GetSupportLanguage(key)
+print('GetSupportLanguage')
+print(result1)
+print()
+
+result2 = DetectLanguage(key, '我想要大便，測試')
+print('DetectLanguage')
+print(result2)
+print()
+
+result3 = Translate(key, '我想要大便，測試', 'zh-TW', 'en')
+print('Traslate')
+print(result3)
+print()
